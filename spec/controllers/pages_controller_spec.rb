e28691f3602dd0dbca853bee705e511314b3ac05
Jason Fiset <jasonf@jmftech.com>
2010-05-16 07:51:51 -0700

    Added blueprint stylesheets

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   integrate_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
   #Delete these examples and add some real ones
   it "should use PagesController" do
     controller.should be_an_instance_of(PagesController)
@@ -18,7 +22,7 @@ describe PagesController do
     it "should have the right title" do 
       get 'home'
       response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | Home")
+                               @base_title + " | Home")
     end
   end
 
@@ -31,7 +35,7 @@ describe PagesController do
     it "should have the right title" do 
       get 'contact'
       response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | Contact")
+                               @base_title + " | Contact")
     end
   end
 
@@ -44,7 +48,20 @@ describe PagesController do
     it "should have the right title" do 
       get 'about'
       response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | About")
+                               @base_title + " | About")
     end
   end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do 
+      get 'help'
+      response.should have_tag("title",
+                               @base_title + " | Help")
+    end
+  end  
 end
